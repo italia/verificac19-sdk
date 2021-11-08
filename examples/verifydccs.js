b89e2d98dd9d5854ee3fb157d439de04f73aaf85
Andrea Stagi <stagi.andrea@gmail.com>
2021-11-08 22:13:40 +0100

    chore: add eu tests

--- examples/verifydccs.js
+++ examples/verifydccs.js
@@ -3,10 +3,10 @@ const { Certificate, Validator } = require('../src');
 const main = async () => {
   try {
     const dccTest = await Certificate.fromImage('./test/data/2.png');
-    const rulesOk = Validator.checkRules(dccTest);
+    const rulesSummary = Validator.checkRules(dccTest);
     const signatureOk = await Validator.checkSignature(dccTest);
-    console.log(`Is this DCC valid? ${rulesOk && signatureOk}`);
-    console.log(rulesOk);
+    console.log(`Is this DCC valid? ${rulesSummary.result && signatureOk}`);
+    console.log(rulesSummary);
     await Certificate.fromImage('./test/data/invalid.png'); // This throws an exception
   } catch (error) {
     console.log(error);
--- test/verification.test.js
+++ test/verification.test.js
@@ -1,8 +1,17 @@
 const chai = require('chai');
-const chaiAsPromised = require('chai-as-promised');
 const { Certificate, Validator } = require('../src');
 
-chai.use(chaiAsPromised);
+chai.use(require('chai-as-promised'));
+chai.use(require('chai-match'));
+
+const verifyRules = async (imagePath, expectedResult, expectedMessageReg = null) => {
+  const dcc = await Certificate.fromImage(imagePath);
+  const rulesReport = Validator.checkRules(dcc);
+  chai.expect(rulesReport.result).to.be.equal(expectedResult);
+  if (expectedMessageReg) {
+    chai.expect(rulesReport.message).to.match(new RegExp(expectedMessageReg));
+  }
+};
 
 const verifyRulesAndSignature = async (imagePath, expectedRules, expectedSignature) => {
   const dcc = await Certificate.fromImage(imagePath);
@@ -14,12 +23,47 @@ const verifyRulesAndSignature = async (imagePath, expectedRules, expectedSignatu
 };
 
 describe('Testing integration between Certificate and Validator', () => {
-  it('rules verification', async () => {
+  it('makes rules verification', async () => {
     await verifyRulesAndSignature('./test/data/shit.png', false, true);
     await verifyRulesAndSignature('./test/data/2.png', false, false);
     await verifyRulesAndSignature('./test/data/example_qr_vaccine_recovery.png', false, false);
     await verifyRulesAndSignature('./test/data/mouse.jpeg', false, true);
     await verifyRulesAndSignature('./test/data/signed_cert.png', false, false);
     await verifyRulesAndSignature('./test/data/uk_qr_vaccine_dose1.png', false, false);
+  });
+
+  it('makes rules verification on SK testing certificates', async () => {
+    await verifyRules(
+      './test/data/eu_test_certificates/SK_1.png', false,
+      '^Doses 1/2 - Vaccination is expired at .*$',
+    );
+    await verifyRules(
+      './test/data/eu_test_certificates/SK_2.png', false,
+      '^Doses 1/2 - Vaccination is expired at .*$',
+    );
+    await verifyRules(
+      './test/data/eu_test_certificates/SK_3.png', true,
+      '^Doses 2/2 - Vaccination is valid .*$',
+    );
+    await verifyRules(
+      './test/data/eu_test_certificates/SK_4.png', true,
+      '^Doses 2/2 - Vaccination is valid .*$',
+    );
+    await verifyRules(
+      './test/data/eu_test_certificates/SK_5.png', true,
+      '^Doses 1/1 - Vaccination is valid .*$',
+    );
+    await verifyRules(
+      './test/data/eu_test_certificates/SK_6.png', false,
+      '^Recovery statement is expired at .*$',
+    );
+    await verifyRules(
+      './test/data/eu_test_certificates/SK_7.png', false,
+      '^Test Result is expired at .*$',
+    );
+    await verifyRules(
+      './test/data/eu_test_certificates/SK_8.png', false,
+      '^Test Result is expired at .*$',
+    );
   });
 });
