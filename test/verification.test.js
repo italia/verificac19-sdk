88ae71ef53aa29f7782edba6ac438ba1acd3f36d
Andrea Stagi <stagi.andrea@gmail.com>
2021-11-08 23:16:53 +0100

    chore: add special cases to tests

--- test/verification.test.js
+++ test/verification.test.js
@@ -1,10 +1,11 @@
 const chai = require('chai');
+const mockdate = require('mockdate');
 const { Certificate, Validator } = require('../src');
 
 chai.use(require('chai-as-promised'));
 chai.use(require('chai-match'));
 
-const verifyRules = async (imagePath, expectedResult, expectedMessageReg = null) => {
+const verifyRulesFromImage = async (imagePath, expectedResult, expectedMessageReg = null) => {
   const dcc = await Certificate.fromImage(imagePath);
   const rulesReport = Validator.checkRules(dcc);
   chai.expect(rulesReport.result).to.be.equal(expectedResult);
@@ -33,37 +34,93 @@ describe('Testing integration between Certificate and Validator', () => {
   });
 
   it('makes rules verification on SK testing certificates', async () => {
-    await verifyRules(
+    await verifyRulesFromImage(
       './test/data/eu_test_certificates/SK_1.png', false,
       '^Doses 1/2 - Vaccination is expired at .*$',
     );
-    await verifyRules(
+    await verifyRulesFromImage(
       './test/data/eu_test_certificates/SK_2.png', false,
       '^Doses 1/2 - Vaccination is expired at .*$',
     );
-    await verifyRules(
+    await verifyRulesFromImage(
       './test/data/eu_test_certificates/SK_3.png', true,
       '^Doses 2/2 - Vaccination is valid .*$',
     );
-    await verifyRules(
+    await verifyRulesFromImage(
       './test/data/eu_test_certificates/SK_4.png', true,
       '^Doses 2/2 - Vaccination is valid .*$',
     );
-    await verifyRules(
+    await verifyRulesFromImage(
       './test/data/eu_test_certificates/SK_5.png', true,
       '^Doses 1/1 - Vaccination is valid .*$',
     );
-    await verifyRules(
+    await verifyRulesFromImage(
       './test/data/eu_test_certificates/SK_6.png', false,
       '^Recovery statement is expired at .*$',
     );
-    await verifyRules(
+    await verifyRulesFromImage(
       './test/data/eu_test_certificates/SK_7.png', false,
       '^Test Result is expired at .*$',
     );
-    await verifyRules(
+    await verifyRulesFromImage(
       './test/data/eu_test_certificates/SK_8.png', false,
       '^Test Result is expired at .*$',
     );
+  });
+
+  it('makes rules verification on special cases', async () => {
+    // Valid test results
+    mockdate.set('2021-05-22T12:34:56.000Z');
+    await verifyRulesFromImage(
+      './test/data/eu_test_certificates/SK_7.png', true,
+      '^Test Result is valid .*$',
+    );
+    await verifyRulesFromImage(
+      './test/data/eu_test_certificates/SK_8.png', true,
+      '^Test Result is valid .*$',
+    );
+    // Doses 1/2 valid only in Italy
+    mockdate.set('2021-06-24T00:00:00.000Z');
+    await verifyRulesFromImage(
+      './test/data/eu_test_certificates/SK_1.png', true,
+      '^Doses 1/2 - Vaccination is valid .*$',
+    );
+    // Test result not valid yet
+    mockdate.set('2021-04-22T12:34:56.000Z');
+    await verifyRulesFromImage(
+      './test/data/eu_test_certificates/SK_7.png', false,
+      '^Test Result is not valid yet, starts at .*$',
+    );
+    // Doses 1/2 not valid yet
+    mockdate.set('2021-05-24T00:00:00.000Z');
+    await verifyRulesFromImage(
+      './test/data/eu_test_certificates/SK_1.png', false,
+      '^Doses 1/2 - Vaccination is not valid yet, .*$',
+    );
+    // Doses 2/2 not valid yet
+    mockdate.set('2021-05-18T00:00:00.000Z');
+    await verifyRulesFromImage(
+      './test/data/eu_test_certificates/SK_3.png', false,
+      '^Doses 2/2 - Vaccination is not valid yet, .*$',
+    );
+    // Doses 2/2 expired
+    mockdate.set('2022-06-17T00:00:00.000Z');
+    await verifyRulesFromImage(
+      './test/data/eu_test_certificates/SK_4.png', false,
+      '^Doses 2/2 - Vaccination is expired at .*$',
+    );
+    // Recovery statement is valid
+    mockdate.set('2021-10-20T00:00:00.000Z');
+    await verifyRulesFromImage(
+      './test/data/eu_test_certificates/SK_6.png', true,
+      '^Recovery statement is valid .*$',
+    );
+    // Recovery statement is not valid yet
+    mockdate.set('2021-04-22T12:34:56.000Z');
+    await verifyRulesFromImage(
+      './test/data/eu_test_certificates/SK_6.png', false,
+      '^Recovery statement is not valid yet, starts at .*$',
+    );
+    mockdate.reset();
   });
 });
