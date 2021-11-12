e850c2e0d8c9edb3d27a838185bea168c76b0ed3
lucadentella <luca@dentella.it>
2021-11-12 15:17:07 +0100

    Review test results based on new recovery rules

--- test/verification.test.js
+++ test/verification.test.js
@@ -32,7 +32,8 @@ describe('Testing integration between Certificate and Validator', () => {
   it('makes rules verification', async () => {
     await verifyRulesAndSignature('./test/data/shit.png', false, true);
     await verifyRulesAndSignature('./test/data/2.png', false, false);
-    await verifyRulesAndSignature('./test/data/example_qr_vaccine_recovery.png', false, false);
+	// recovery, start=10/08/2021 (VALID)
+    await verifyRulesAndSignature('./test/data/example_qr_vaccine_recovery.png', true, false);
     await verifyRulesAndSignature('./test/data/mouse.jpeg', false, true);
     await verifyRulesAndSignature('./test/data/signed_cert.png', false, false);
     await verifyRulesAndSignature('./test/data/uk_qr_vaccine_dose1.png', false, false);
@@ -66,8 +67,8 @@ describe('Testing integration between Certificate and Validator', () => {
     );
     await verifyRulesFromImage(
       './test/data/eu_test_certificates/SK_6.png', false,
-      Validator.codes.NOT_VALID,
-      '^Recovery statement is expired at .*$',
+      Validator.codes.VALID,
+      '^Recovery statement is valid .*$',
     );
     await verifyRulesFromImage(
       './test/data/eu_test_certificates/SK_7.png', false,
