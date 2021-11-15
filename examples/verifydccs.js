7dbd9d77d40ea7b6558a59baf0ecbb247522d400
Andrea Stagi <stagi.andrea@gmail.com>
2021-11-15 17:33:22 +0100

    chore: update examples

--- examples/verifydccs.js
+++ examples/verifydccs.js
@@ -5,9 +5,15 @@ const main = async () => {
     const dccTest = await Certificate.fromImage('./test/data/2.png');
     const validationResult = await Validator.validate(dccTest);
     console.log(`Is this DCC valid? ${validationResult.result}`);
+    console.log('Details:');
     console.log(validationResult);
     console.log(validationResult.code === Validator.codes.NOT_VALID);
-    await Certificate.fromImage('./test/data/invalid.png'); // This throws an exception
+  } catch (error) {
+    console.log(error);
+  }
+
+  try {
+    await Certificate.fromImage('./test/data/invalid.png'); // Throws an exception
   } catch (error) {
     console.log(error);
   }
