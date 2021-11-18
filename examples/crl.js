6ef59fd4e7ba02448e0877ae3a3b0b82e4ca9547
Andrea Stagi <stagi.andrea@gmail.com>
2021-11-18 18:16:15 +0100

    chore: update crl example

--- examples/crl.js
+++ examples/crl.js
@@ -1,13 +1,11 @@
 const { Service } = require('../src');
+const cache = require('../src/cache');
 
 const main = async () => {
   await Service.setUp();
-  console.log('Version 0');
+  console.log('Version 0, 1 and 2');
   await Service.updateCRL();
-  console.log('Version 1');
-  await Service.updateCRL();
-  console.log('Version 2');
-  await Service.updateCRL();
+  console.log(await cache.isUVCIRevoked('asd'));
   await Service.tearDown();
 };
 
