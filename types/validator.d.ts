df2f369def2cc103e1e38d5ec1f8997e3fae25d3
Andrea Stagi <stagi.andrea@gmail.com>
2021-12-02 18:37:37 +0100

    fix: add mode to types

--- types/validator.d.ts
+++ types/validator.d.ts
@@ -2,17 +2,20 @@ import { RulesResult, DCCCertificate, ValidateGP } from "./models";
 
 declare module Validator {
   function checkSignature(certificate: DCCCertificate): Promise<boolean>;
-  function checkRules(certificate: DCCCertificate): RulesResult;
-  function validate(certificate: DCCCertificate): Promise<ValidateGP>
+  function checkRules(certificate: DCCCertificate, mode?: Validator.mode): RulesResult;
+  function validate(certificate: DCCCertificate, mode?: Validator.mode): Promise<ValidateGP>
 
-  namespace codes {
-    const VALID: "VALID";
-    const PARTIALLY_VALID: "PARTIALLY_VALID";
-    const NOT_VALID: "NOT_VALID";
-    const NOT_VALID_YET: "NOT_VALID_YET";
-    const NOT_EU_DCC: "NOT_EU_DCC";
+  enum codes {
+    VALID = "VALID",
+    PARTIALLY_VALID = "PARTIALLY_VALID",
+    NOT_VALID = "NOT_VALID",
+    NOT_VALID_YET = "NOT_VALID_YET",
+    NOT_EU_DCC = "NOT_EU_DCC"
+  }
 
-    export { VALID, PARTIALLY_VALID, NOT_VALID, NOT_VALID_YET, NOT_EU_DCC };
+  enum mode {
+    SUPER_DGP = '2G',
+    NORMAL_DGP = '3G'
   }
 }
 
