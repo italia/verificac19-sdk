4e95ecf4bb657831fb2482b49c21021811bca5a4
Andrea Stagi <stagi.andrea@gmail.com>
2022-01-14 09:37:43 +0100

    fix: possible memory leak solution

--- src/crl.js
+++ src/crl.js
@@ -1,11 +1,13 @@
 const mongoose = require('mongoose');
 
+const MODEL_NAME = 'UVCI';
+
 class CRL {
   async setUp() {
     this._dbConnection = await mongoose.createConnection(
       process.env.VC19_MONGODB_URL || 'mongodb://root:example@localhost:27017/VC19?authSource=admin',
     );
-    this._dbModel = this._dbConnection.model('UVCI', new mongoose.Schema({
+    this._dbModel = this._dbConnection.model(MODEL_NAME, new mongoose.Schema({
       _id: String,
     }));
   }
@@ -38,6 +40,10 @@ class CRL {
   async tearDown() {
     if (this._dbConnection) {
       await this._dbConnection.close();
+      delete this._dbConnection.models[MODEL_NAME];
+      delete this._dbConnection.collections.uvcis;
+      await this._dbConnection.deleteModel(/.+/);
+      await mongoose.deleteModel(/.+/);
     }
   }
 
