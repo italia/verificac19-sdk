82de821e51c2d55fc5bb074459660f2f2652c77c
Andrea Stagi <stagi.andrea@gmail.com>
2022-03-21 18:04:29 +0100

    fix: verification moment

--- src/validator.js
+++ src/validator.js
@@ -143,11 +143,9 @@ const checkVaccinations = (certificate, rules, mode) => {
       };
     }
 
-    const startNow = new Date(Date.now());
-    const endNow = new Date(Date.now());
+    const verificationMoment = new Date(Date.now());
 
-    startNow.setUTCHours(0, 0, 0, 0);
-    endNow.setUTCHours(23, 59, 59, 999);
+    verificationMoment.setUTCHours(0, 0, 0, 0);
 
     let startDate = new Date(
       Date.parse(clearExtraTime(last.dateOfVaccination)),
@@ -402,7 +400,7 @@ const checkVaccinations = (certificate, rules, mode) => {
     endDate = addDays(endDate, vaccineEndDay.value);
 
     // Not valid yet
-    if (startDate > endNow) {
+    if (startDate > verificationMoment) {
       return {
         code: NOT_VALID_YET,
         message:
@@ -413,7 +411,7 @@ const checkVaccinations = (certificate, rules, mode) => {
     }
 
     // Valid only if no test is required
-    if (endNow <= endDate) {
+    if (verificationMoment <= endDate) {
       if (testRequired) {
         return {
           code: TEST_NEEDED,
@@ -433,7 +431,7 @@ const checkVaccinations = (certificate, rules, mode) => {
     }
 
     // Test needed in case of extension
-    if (vaccineEndDayExtended && endNow < addDays(endDate, vaccineEndDayExtended.value)) {
+    if (vaccineEndDayExtended && verificationMoment < addDays(endDate, vaccineEndDayExtended.value)) {
       return {
         code: TEST_NEEDED,
         message: 'Test needed',
@@ -441,7 +439,7 @@ const checkVaccinations = (certificate, rules, mode) => {
     }
 
     // Not valid if expired
-    if (startNow > endDate) {
+    if (verificationMoment > endDate) {
       return {
         code: NOT_VALID,
         message:
